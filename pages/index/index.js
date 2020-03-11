092ebe20041123ab2599434b1781f356cfa5d525
stephen luo <xingzhang_luo@163.com>
2020-03-11 16:12:25 +0800

    实现了课程搜索功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -28,13 +28,45 @@ Page({
     }], // 弹出窗口按钮选项
     loginshow: false,
 
-  
+    /** 课程检索数据 */
+    initvalue: '',
+    contentshow: 'none', // 用于前台数据的显示
+    arr: [], // 用于存储满足条件的数据
+    courses: ['html', 'css', 'javascript', 'jquery', 'node', 'gulp', 'mongo', 'express'], // 存储课程的集合
   },
 
   /**
-   * searchbar组件的相关处理
+   * input的相关处理
    */
-  
+  searchInput: function(e) {
+    let that = this;
+    var inputvalue = e.detail.value;
+    var courses = this.data.courses;
+    var arr = [];
+    // 判断数据中的字符串是否含有该字符
+    if (inputvalue.trim().length != 0) {
+      // 当输入值不为空
+      courses.forEach(str => {
+        // 使用定时器能够提高用户体验以及性能
+        return setTimeout(() => {
+          if (str.search(inputvalue) != -1) {
+            arr.push(str);
+          }
+          that.setData({
+            arr: arr,
+            contentshow: 'block'
+          })
+        }, 200)
+      })
+    }
+    if (inputvalue.trim().length == 0) {
+      // 当输入值为空时
+      that.setData({
+        arr: [],
+        contentshow: 'none',
+      })
+    }
+  },
 
   /**
    * 跳转到视频播放
@@ -68,7 +100,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    
+
   },
 
   /**
@@ -82,8 +114,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
+    // 将相关的数据初始化（dialog、搜索框的值）
+    var initvalue = this.data.initvalue;
     this.setData({
       loginshow: false,
+      initvalue: '',
+      contentshow: 'none',
     })
   },
 
